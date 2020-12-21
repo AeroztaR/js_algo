81eaa2cbd97a3c448105e142db38e8d07054e540
AeroztaR <jorguinhos89@gmail.com>
2020-12-21 06:33:11 +0500

    Added oneline method with spread operator

--- js/anagram.js
+++ js/anagram.js
@@ -1,46 +1,57 @@
 const anagram = () => {
-    // first method
-    function getAnagrams(stringA, stringB) {
-        const aCharMap = createCharMap(stringA);
-        const bCharMap = createCharMap(stringB);
+	// first method
+	function getAnagrams(stringA, stringB) {
+		const aCharMap = createCharMap(stringA);
+		const bCharMap = createCharMap(stringB);
 
-        if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
-            return false;
-        } 
+		if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+			return false;
+		}
 
-        for (let char in aCharMap) {
-            if (aCharMap[char] !== bCharMap[char]) {
-                return false;
-            } 
-        }
+		for (let char in aCharMap) {
+			if (aCharMap[char] !== bCharMap[char]) {
+				return false;
+			}
+		}
 
-        return true;
-    }
+		return true;
+	}
 
-    function createCharMap(str) {
-        const charMap = {};
-        
-        for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
-            charMap[char] = charMap[char] + 1 || 1;
-        }
+	function createCharMap(str) {
+		const charMap = {};
 
-        return charMap;
-    }
+		for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+			charMap[char] = charMap[char] + 1 || 1;
+		}
 
-    // second method
-    function getAnotherAnagrams(strA, strB) {
-        return cleanString(strA) === cleanString(strB);
-    }
+		return charMap;
+	}
 
-    function cleanString(string) {
-        return string.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
-    }
+	// second method
+	function getAnotherAnagrams(strA, strB) {
+		return cleanString(strA) === cleanString(strB);
+	}
 
-    console.log(getAnagrams('barcelona', 'balancero'));
-    console.log(getAnagrams('barcelona', 'barnaelc'));
-    console.log(getAnotherAnagrams('nice', 'ecine'));
-    console.log(getAnotherAnagrams('code', 'deco'));
+	function cleanString(string) {
+		return string
+			.replace(/[^\w]/g, '')
+			.toLowerCase()
+			.split('')
+			.sort()
+			.join('');
+	}
 
+	// third method
+	const getSomeAnagrams = (a, b) =>
+		[...a.toLowerCase()].sort().toString() ===
+		[...b.toLowerCase()].sort().toString();
+
+	console.log(getAnagrams('barcelona', 'balancero'));
+	console.log(getAnagrams('barcelona', 'barnaelc'));
+	console.log(getAnotherAnagrams('nice', 'ecine'));
+	console.log(getAnotherAnagrams('code', 'deco'));
+	console.log(getSomeAnagrams('bebe', 'ebeb'));
+	console.log(getSomeAnagrams('vivo', 'ovive'));
 };
 
 export default anagram;
